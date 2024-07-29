b0ed23baaeedd5104fa29d8059f58ae354af4464
BeatriceLin07 <beatricelin07@gmail.com>
2024-07-29 12:26:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -142,5 +142,6 @@ def handle_message(event):
         line_bot_api.reply_message_with_http_info(reply_request)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
